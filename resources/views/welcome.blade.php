93ea40a44b13b51aa519e184de28f576dc7bb786
fianbong2014 <fianbong.f@gmail.com>
2019-07-26 20:34:38 +0700

    Test header : Demo Laravel test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Demo laravel
+                    Demo laravel Test
                 </div>
 
                 <div class="links">
